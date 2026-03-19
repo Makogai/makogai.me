4fe7de90797afa0c0f56a183fa378625c8d3cbb1
Marko <mare.sampbn@gmail.com>
2026-03-19 09:57:12 +0100

    add fillable to projects

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -3,16 +3,44 @@
 namespace App\Models;
 
 use Database\Factories\ProjectFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+#[Fillable([
+    'user_id',
+    'title',
+    'slug',
+    'description',
+    'content_markdown',
+    'content_html',
+    'cover_image_path',
+    'gallery',
+    'tech_stack',
+    'repo_url',
+    'demo_url',
+    'is_featured',
+    'published_at',
+    'seo_title',
+    'seo_description',
+])]
 class Project extends Model
 {
     /** @use HasFactory<ProjectFactory> */
     use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'title',
+        'slug',
+        'description',
+        'content_markdown',
+        'content_html',
+        'cover_image_path',
+    ];
 
     /**
      * @return BelongsTo<User, $this>
